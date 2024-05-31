7250b18d832e94b34505dd4986930293e8815acf
carolina <lecahipe@gmail.com>
2024-05-31 09:42:55 -0500

    fix avg field error

--- django_caroma/api/filters.py
+++ django_caroma/api/filters.py
@@ -5,8 +5,8 @@ from urllib.parse import unquote
 class RestaurantFilter(django_filters.FilterSet):
     postal_codes = django_filters.CharFilter(method='filter_postal_codes', label='Postal Codes')
     exclude_types = django_filters.CharFilter(method='filter_exclude_types', label='Exclude Restaurant Type')
-    min_rating = django_filters.NumberFilter(field_name='rating__avg', lookup_expr='gte', label='Minimum Rating')
-    max_rating = django_filters.NumberFilter(field_name='rating__avg', lookup_expr='lte', label='Maximum Rating')
+    min_rating = django_filters.NumberFilter(field_name='ratings__avg', lookup_expr='gte', label='Minimum Rating')
+    max_rating = django_filters.NumberFilter(field_name='ratings__avg', lookup_expr='lte', label='Maximum Rating')
    
     class Meta:
         model = Restaurant
